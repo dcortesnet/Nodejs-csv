c84d91d08a9dc43c6f958ab9e97cec1535193ee8
dcortesnet <dcortes.net@gmail.com>
2024-03-25 21:47:56 -0300

    Refactor CSV parsing code to trim and skip empty lines

--- read/read.js
+++ read/read.js
@@ -1,11 +1,14 @@
 const fs = require("fs");
 const csv = require("csv");
 
+const results = [];
+
 fs.createReadStream("sample.csv")
-  .pipe(csv.parse())
-  .on("data", (row) => {
-    console.log(row);
-  })
-  .on("end", () => {
-    console.log("CSV parsing complete");
-  });
+    .pipe(csv.parse({trim: true, skip_empty_lines: true, columns: true}))
+    .on("data", (row) => {
+        results.push(row);
+    })
+    .on("end", () => {
+        console.log(results);
+        console.log("CSV parsing complete");
+    });
